11716f67e70f3d553c0617db52f3ef3a266e6b0f
Vladimir Evseev <vladimir.yevseev@gmail.com>
2024-02-25 19:54:05 +1000

    Update sentry-create-release.js

--- other/sentry-create-release.js
+++ other/sentry-create-release.js
@@ -8,10 +8,7 @@ const DEFAULT_BUILD_PATH = 'build';
 
 // exit with non-zero code if we have everything for Sentry
 if (
-	process.env.SENTRY_DSN &&
-	process.env.SENTRY_ORG &&
-	process.env.SENTRY_PROJECT &&
-	process.env.SENTRY_AUTH_TOKEN
+	false
 ) {
 	createRelease({}, DEFAULT_URL_PREFIX, DEFAULT_BUILD_PATH);
 } else {
