c2d3aec07ce59d062113b136bd7dc3c852b69dfa
vVolodya <vladimir.yevseev@gmail.com>
2024-02-27 16:37:31 +1000

    :bug: fix(ui): theme switcher

--- app/shared/lib/hooks/useTheme/useTheme.tsx
+++ app/shared/lib/hooks/useTheme/useTheme.tsx
@@ -1,5 +1,6 @@
 import { useHints } from '#app/core/client-utils/clientHints/clientHints.tsx';
 import { useOptimisticThemeMode } from '../useOptimisticThemeMode/useOptimisticThemeMode.tsx';
+import { useRequestInfo } from '../useRequestInfo/useRequestInfo.tsx';
 
 /**
  * @returns the user's theme preference, or the client hint theme if the user
@@ -7,11 +8,12 @@ import { useOptimisticThemeMode } from '../useOptimisticThemeMode/useOptimisticT
  */
 export function useTheme() {
 	const hints = useHints();
+	const requestInfo = useRequestInfo();
 	const optimisticMode = useOptimisticThemeMode();
 
 	if (optimisticMode) {
 		return optimisticMode === 'system' ? hints.theme : optimisticMode;
 	}
 
-	return hints.theme;
+	return requestInfo.userPrefs.theme ?? hints.theme;
 }
